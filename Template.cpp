8480bd4bd58fdce5b4d9aefdde5a8e9df7dcfe44
Sobuj Chandra Paul <146801317+Sobujpaul@users.noreply.github.com>
2026-08-26 09:47:20 +0600

    Create Template.cpp

---
+++ Template.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+#define lng long long
+using namespace __gnu_pbds;
+using namespace std;
+template <typename T> using o_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int32_t main(){
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  cout.tie(nullptr);
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    
+  }
+  return 0;
+}
